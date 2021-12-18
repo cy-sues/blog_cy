88e716e7856fc34ff7a8743e0ba668a8fd770a69
ccc_38451 <chenyang3817@163.com>
2021-12-18 16:28:02 +0800

    test提交

---
+++ src/main/java/com/suse/blog_cy/entity/ccc.java
@@ -0,0 +1,22 @@
+package com.suse.blog_cy.entity;
+
+public class ccc {
+    private int tsa;
+    private String aa;
+
+    public int getTsa() {
+        return tsa;
+    }
+
+    public void setTsa(int tsa) {
+        this.tsa = tsa;
+    }
+
+    public String getAa() {
+        return aa;
+    }
+
+    public void setAa(String aa) {
+        this.aa = aa;
+    }
+}
